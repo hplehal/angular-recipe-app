3d103f4171fce48fc0e53b30809c3ea561261045
Tej Lehal <tejlehal@Tejs-MacBook-Air.local>
2019-10-21 13:41:52 -0400

    added link to edit and new recipe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from "@angular/core";
 
 import { RouterModule } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeAddedComponent } from './recipes/recipe-added/recipe-added.component';
 
 const appRoute = [
     { path:'', redirectTo: '/recipes', pathMatch: 'full' },
     { path: 'recipes', component: RecipesComponent, children: [
         {path:'', component: RecipeStartComponent},
-        {path:':id', component: RecipeDetailComponent}
+        {path:'new', component: RecipeAddedComponent},
+        {path:':id', component: RecipeDetailComponent},
+        {path:':id/edit', component: RecipeAddedComponent},
     ]},
     { path:'shopping-list', component: ShoppingListComponent }
   ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeAddedComponent } from './recipes/recipe-added/recipe-added.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
     RecipeItemComponent,
     ShoppingListEditComponent,
     DropdownDirective,
-    RecipeStartComponent
+    RecipeStartComponent,
+    RecipeAddedComponent
   ],
   imports: [
     BrowserModule,
